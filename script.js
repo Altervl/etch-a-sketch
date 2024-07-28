80d3932180eabc9e6c88b7309a24c96bc5ed00ed
Altervl <62514019+Altervl@users.noreply.github.com>
2024-07-28 20:48:15 +0300

    Add a draw functionality
    Implement drawing by one color with a progressive darkening effect.

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let canvas = document.createElement("div");
-canvas.setAttribute("id", "container");
-document.body.appendChild(canvas);
+let canvas = document.querySelector("#container");
 
 let side = prompt("Enter a side length from 2 to 100:");
-let size = side**2;
+let canvas_size = side**2;
 
 let pixel_size = canvas.offsetWidth / side;
 
-for (i = 0; i < size; i++) {
+for (i = 0; i < canvas_size; i++) {
     let pixel = document.createElement("div");
     pixel.className = "pixel";
     pixel.style.width = `${pixel_size}px`;
     pixel.style.height = `${pixel_size}px`;
     canvas.appendChild(pixel);
 };
+
+canvas.addEventListener("mouseover", (event) => {
+    let target = event.target;
+
+    if (target.className === "pixel") {
+        target.className = "painted";
+        target.style.opacity = "0.1";
+    } else {
+        target.style.opacity -= "-0.1";
+    };
+});
