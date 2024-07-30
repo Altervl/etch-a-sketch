389c4067efdb96a10d3ae00a22d38af4b0ed0b8c
Altervl <62514019+Altervl@users.noreply.github.com>
2024-07-30 16:19:51 +0300

    Add some buttons, remove the grid
    "clear" button removes all colors from the canvas.  Grid lines deleted.

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 let canvas = document.querySelector("#container");
+let clear = document.querySelector("#clear");
 
 let side = prompt("Enter a side length from 2 to 100:");
 let canvas_size = side**2;
 
-let pixel_size = canvas.offsetWidth / side;
+let blank_size = canvas.offsetWidth / side;
 
-for (i = 0; i < canvas_size; i++) {
-    let pixel = document.createElement("div");
-    pixel.className = "pixel";
-    pixel.style.width = `${pixel_size}px`;
-    pixel.style.height = `${pixel_size}px`;
-    canvas.appendChild(pixel);
+for (i = 1; i <= canvas_size; i++) {
+    let blank = document.createElement("div");
+    blank.className = "blank";
+    blank.style.width = `${blank_size}px`;
+    blank.style.height = `${blank_size}px`;
+    canvas.appendChild(blank);
 };
 
 canvas.addEventListener("mouseover", (event) => {
     let target = event.target;
 
-    if (target.className === "pixel") {
+    if (target.className === "blank") {
         target.className = "painted";
-        target.style.opacity = "0.1";
+        target.style.opacity = "0.5";
     } else {
-        target.style.opacity -= "-0.1";
+        target.style.opacity -= "-0.05"; // "+=" tries to process values as stings
+    };
+});
+
+clear.addEventListener("click", () => {
+    for (const child of canvas.children) {
+        child.className = "blank";
     };
 });
